785efb8e1afd5c41ba868fd7d66a67e2fae3a5de
vagrant <vagrant@ubuntu1804.localdomain>
2021-12-02 02:01:46 +0000

    fixed the Opcoed

--- ridikkulus_router.py
+++ ridikkulus_router.py
@@ -104,7 +104,10 @@ class SimpleRouter(SimpleRouterBase):
 
         logVerboseMessage("Checkpoint 1: processArp function")
         pkt = headers.ArpHeader(arpPacket)
-        if pkt.Opcode == 1:
+        logVerboseMessage("Checkpoint 1.1: processArp function, pkt information: ")
+        logVerboseMessage(str(pkt))
+        logVerboseMessage("/n pkt.op" + str(pkt.op))
+        if pkt.op == 1:
             logVerboseMessage("Checkpoint 2: processArp function")
             # Then it is a request
             destIP = pkt.tip
@@ -112,11 +115,11 @@ class SimpleRouter(SimpleRouterBase):
             if requestIP is not None:
                 logVerboseMessage("Checkpoint 3: processArp function")
                 pkt.tha = requestIP.mac
-                pkt.Opcode = 2
+                pkt.op = 2
                 offset = pkt.decode(arpPacket)
                 logVerboseMessage("Checkpoint 4: processArp function")
                 new_packet = pkt.encode() + arpPacket[offset:]
-        elif pkt.Opcode == 2:
+        elif pkt.op == 2:
             logVerboseMessage("Checkpoint 5: processArp function")
             self.arpCache.handleIncomingArpReply(pkt)
         else:
